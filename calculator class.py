0b5f98fd25346c63bf909aaf670c432aa3c605cd
vanishreearsu <124886581+vanishreearsu@users.noreply.github.com>
2023-03-20 21:38:30 +0530

    Add files via upload

---
+++ calculator class.py
@@ -0,0 +1,25 @@
+class Calculator:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def add(self):
+        pass
+        return self.num1 + self.num2
+
+    def subtract(self):
+        pass
+        return self.num2 - self.num1
+
+    def multiply(self):
+        pass
+        return self.num1 * self.num2
+
+    def divide(self):
+        pass
+        return self.num2 / self.num1
+obj = Calculator(10, 94)
+print(obj.add())       
+print(obj.subtract())  
+print(obj.multiply())  
+print(obj.divide())    
